4815ceb2f4cbd6848062d2d3289aea8611cd3761
HuniHuns <i690367@gmail.com>
2025-05-02 12:59:41 +0900

    project init

---
+++ src/main/java/bonda/bonda/BondaApplication.java
@@ -0,0 +1,13 @@
+package bonda.bonda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BondaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bonda/bonda/BondaApplicationTests.java
@@ -0,0 +1,13 @@
+package bonda.bonda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
