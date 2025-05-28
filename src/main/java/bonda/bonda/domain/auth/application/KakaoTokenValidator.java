f867d7af33c8a528884fc85c101d965e681a48d1
HuniHuns <i690367@gmail.com>
2025-05-28 11:24:04 +0900

    [FEAT] kakao의 idToken 검증

---
+++ src/main/java/bonda/bonda/domain/auth/application/KakaoTokenValidator.java
@@ -0,0 +1,39 @@
+package bonda.bonda.domain.auth.application;
+
+import bonda.bonda.domain.auth.dto.response.KakaoMemberRes;
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.UrlJwkProvider;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.net.URL;
+import java.security.interfaces.RSAPublicKey;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoTokenValidator {
+
+    public KakaoMemberRes validateAndExtract(String idToken) {
+        try {
+            JwkProvider jwkProvider = new UrlJwkProvider(new URL("https://kauth.kakao.com/.well-known/jwks.json"));
+            DecodedJWT decodedJWT = JWT.decode(idToken);
+
+            Jwk jwk = jwkProvider.get(decodedJWT.getKeyId());
+            Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            verifier.verify(idToken);
+
+            String sub = decodedJWT.getClaim("sub").asString();
+
+            return new KakaoMemberRes(sub);
+        } catch (Exception e) {
+            throw new RuntimeException("유효하지 않은 kakao_idToken 입니다." + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/bonda/bonda/domain/auth/dto/response/KakaoMemberRes.java
@@ -0,0 +1,11 @@
+package bonda.bonda.domain.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class KakaoMemberRes {
+
+    private String kakaoId;
+}
