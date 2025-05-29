1f24c4d7546ece5ab901c8c20a8cf6dcfd3885a0
HuniHuns <i690367@gmail.com>
2025-05-29 12:04:38 +0900

    [REFACTOR] jwt filter 추가

--- src/main/java/bonda/bonda/global/config/SecurityConfig.java
+++ src/main/java/bonda/bonda/global/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -42,7 +43,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(WHITE_LIST).permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationProcessingFilter(), LogoutFilter.class)
+        ;
 
         return http.build();
     }
