67f6e85f3aca09fdc58105b6098dd18281dcc79c
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-06-21 19:09:07 +0900

    [FEAT] 아티클 저장 기능 구현 완료

--- src/main/java/bonda/bonda/domain/articlecase/repository/ArticlecaseRepository.java
+++ src/main/java/bonda/bonda/domain/articlecase/repository/ArticlecaseRepository.java
@@ -1,5 +1,6 @@
 package bonda.bonda.domain.articlecase.repository;
 
+import bonda.bonda.domain.article.domain.Article;
 import bonda.bonda.domain.articlecase.Articlecase;
 import bonda.bonda.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ArticlecaseRepository extends JpaRepository<Articlecase, Long> {
 
     int countByMember(Member member);
+
+    boolean existsByMemberAndArticle(Member persistMember, Article article);
 }
