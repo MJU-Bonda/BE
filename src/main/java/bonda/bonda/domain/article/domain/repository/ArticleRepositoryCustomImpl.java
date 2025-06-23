a831c2a38a1bbc819ad8ab9696ffc4b2453cf73a
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-06-23 14:39:24 +0900

    [FEAT] 아티클 검색 레포지토리단 구현 완료

--- src/main/java/bonda/bonda/domain/article/domain/repository/ArticleRepositoryCustomImpl.java
+++ src/main/java/bonda/bonda/domain/article/domain/repository/ArticleRepositoryCustomImpl.java
@@ -5,9 +5,13 @@ import bonda.bonda.domain.article.domain.ArticleCategory;
 import bonda.bonda.domain.article.domain.QArticle;
 import bonda.bonda.domain.article.dto.response.SimpleArticleResWithBookmarked;
 import bonda.bonda.domain.articlecase.QArticlecase;
+import bonda.bonda.domain.book.domain.QBook;
+import bonda.bonda.domain.bookarticle.QBookArticle;
 import bonda.bonda.domain.member.domain.Member;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +20,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -24,6 +29,8 @@ public class ArticleRepositoryCustomImpl implements ArticleRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
     private final QArticle article = QArticle.article;
     private final QArticlecase articlecase = QArticlecase.articlecase;
+    private final QBookArticle bookArticle = QBookArticle.bookArticle;
+    private final QBook book = QBook.book;
 
     @Override
     public Page<SimpleArticleResWithBookmarked> findArticleListByCategory(Pageable pageable, String category, Member loginMember) {
@@ -61,10 +68,102 @@ public class ArticleRepositoryCustomImpl implements ArticleRepositoryCustom {
         return new PageImpl<>(articles, pageable, total != null ? total : 0);
     }
 
+    /**
+     *  아티클 리스트를 검색어로 검색합니다. 검색어 조근을 만들어서, 정렬 처리 해주는 메서드를 반환합니다.
+     * **/
     @Override
     public Page<Article> searchArticleList(Pageable pageable, String orderBy, String word) {
-        return null;
+        if (word == null || word.isBlank()) { // 빈칸이거나 공백이면 결과 없음 처리
+            return new PageImpl<>(List.of(), pageable, 0);
+        }
+        BooleanBuilder predicate = new BooleanBuilder();
+
+        // 검색 조건 추가
+        Arrays.stream(word.trim().split("\\s+")) // 앞 뒤 공백 지우고, 공백 기준 쪼개기
+                .forEach(w -> { // 단어 돌면서
+                    BooleanBuilder wordPredicate = new BooleanBuilder();
+                    wordPredicate.or(article.title.containsIgnoreCase(w)) //아티클 제목
+                            .or(article.content.containsIgnoreCase(w)) // 아티클 내용
+                            .or(article.introduction.containsIgnoreCase(w)) // 아티클 소개글
+                            .or(book.title.containsIgnoreCase(w)); // 연관된 책의 제목
+                    predicate.and(wordPredicate);
+                });
+
+        //orderBy에 따라 메서드 분리
+        return fetchArticlePage(pageable, predicate, orderBy, null);
     }
 
+    /**
+     *
+     * orderBy 속성에 따라서 메서드를 분기 처리해줍니다.
+     */
+    private Page<Article> fetchArticlePage(Pageable pageable, BooleanBuilder predicate, String orderBy, Member loginMember) {
+        List<Article> content = switch (orderBy.toLowerCase()) {
+            case "popularity" -> // 인기순 정렬
+                    fetchArticlesByPopularity(pageable, predicate);
+            default -> // newest
+                    fetchArticlesByNewest(pageable, predicate);
+        };
+        return createPage(content, pageable, predicate, loginMember);
+    }
+
+    /**
+     * 저장한 아티클과 추가 조인해서, 아티클을 최신순으로 정렬합니다.
+     */
+    private List<Article> fetchArticlesByPopularity(Pageable pageable, BooleanBuilder predicate) {
+        List<Tuple> tuples = jpaQueryFactory
+                .select(article, articlecase.count().as("savedCount")) // 중복 제거
+                .from(article)
+                .leftJoin(bookArticle).on(bookArticle.article.eq(article)) // 아티클 기준 조인
+                .leftJoin(bookArticle.book, book) // 도서 조인
+                .leftJoin(articlecase).on(articlecase.article.eq(article))
+                .where(predicate)
+                .groupBy(article.id)
+                .orderBy(articlecase.count().desc(),
+                        Expressions.numberTemplate(Double.class, "rand()").asc()) // 같으면 랜덤 정렬
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        return tuples.stream()
+                .map(t -> t.get(article))
+                .toList();
+    }
+
+
+    /**
+     *  최근 생성일 기준으로 정렬하여 아티클 리스트를 조회합니다.
+     * **/
+    private List<Article> fetchArticlesByNewest(Pageable pageable, BooleanBuilder predicate) {
+        return jpaQueryFactory
+                .selectDistinct(article) // 중복 제거
+                .from(article)
+                .leftJoin(bookArticle).on(bookArticle.article.eq(article)) // 아티클 기준 조인
+                .leftJoin(bookArticle.book, book) // 도서 조인
+                .where(predicate)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(article.id.desc())
+                .fetch();
+    }
+
+    /**
+     *  pageable 정보를 받아서, next page 여부를 확인하기 위해 count() 쿼리를 추가로 보냅니다.
+     * **/
+    private Page<Article> createPage(List<Article> content, Pageable pageable, BooleanBuilder predicate, Member loginMember) {
+        BooleanBuilder totalPredicate = new BooleanBuilder(predicate);
+        // 현재 회원이 선택한 것만 필요할 때, 조건 추가
+        if (loginMember != null) {
+            totalPredicate.and(articlecase.member.eq(loginMember));
+        }
+
+        Long total = jpaQueryFactory
+                .select(book.count())
+                .from(book)
+                .leftJoin(articlecase).on(articlecase.article.eq(article))  // bookcase 조인 필수!
+                .where(totalPredicate)
+                .fetchOne();
+
+        return new PageImpl<>(content, pageable, total != null ? total : 0);
+    }
 
 }
