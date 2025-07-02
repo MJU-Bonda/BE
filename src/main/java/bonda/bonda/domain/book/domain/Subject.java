f14df97b7db8c6a3cbc8411a128a131ca0702cd7
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-07-02 13:22:02 +0900

    [CHORE] 도서 주제 수정

--- src/main/java/bonda/bonda/domain/book/domain/Subject.java
+++ src/main/java/bonda/bonda/domain/book/domain/Subject.java
@@ -11,19 +11,18 @@ public enum Subject {
 
 
     ALL("ALL", "전체"),
-    COOKING("COOKING","요리"),
-    SELF_DEVELOPMENT("SELF_DEVELOPMENT","자기계발"),
-    TRAVEL("TRAVEL","로컬/여행"),
-    MINIMALISM("MINIMALISM","미니멀리즘"),
-    MOVIE("MOVIE","영화"),
-    PET("PET","반려동물"),
-    INTERIOR("INTERIOR","인테리어"),
-    COFFEE("COFFEE","커피"),
-    VEGAN("VEGAN","비건"),
-    FITNESS("FITNESS","운동"),
-    PLANT("PLANT","반려식물"),
-    BURNOUT("BURNOUT","번아웃"),
-    MUSIC("MUSIC","음악");
+    COOKING("COOKING", "음식/요리"),
+    SELF_DEVELOPMENT("SELF_DEVELOPMENT", "자기계발"),
+    ART("ART", "예술"),
+    SPACE("SPACE", "공간"),
+    MOVIE("MOVIE", "영화"),
+    PET("PET", "반려동물"),
+    LOVE("LOVE", "사랑"),
+    COFFEE("COFFEE", "커피"),
+    COMFORT("COMFORT", "위로"),
+    PLANT("PLANT", "자연/식물"),
+    BURNOUT("BURNOUT", "번아웃"),
+    MUSIC("MUSIC", "음악");
 
     final private String key;
     final private String value;
